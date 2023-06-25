b99a0f728272084810a9649438adf7d9e0942b96
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2023-06-26 02:18:07 +0530

    Add files via upload

---
+++ day10/PrintPermutationString.java
@@ -0,0 +1,28 @@
+package recursionAndBacktracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <a href="https://leetcode.com/problems/permutations/">Problem</a>
+ **/
+public class Permutations {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        fill(nums, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    private void fill(int[] nums, List<Integer> cur, List<List<Integer>> ans) {
+        if (cur.size() == nums.length) {
+            ans.add(cur);
+            return;
+        }
+        for (Integer i : nums) {
+            if (cur.contains(i)) continue;
+            var cloned = new ArrayList<>(cur);
+            cloned.add(i);
+            fill(nums, cloned, ans);
+        }
+    }
+}
